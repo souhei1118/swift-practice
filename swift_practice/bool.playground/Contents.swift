53fe1f71ebf33ad71d5d4083f6996eacf182944a
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-04 18:07:02 +0900

    [finish]swift2-8

--- swift_practice/bool.playground/Contents.swift
+++ swift_practice/bool.playground/Contents.swift
@@ -1,5 +1,24 @@
 // 真理値を示すBool型について
 let hoge = true
 let fuga = false
-print(hoge)
-print(fuga)
+
+// 論理演算
+// 否定
+let isMyAccount = true
+let isNotMyAccount = !isMyAccount
+
+// 論理値
+//let one = false && false
+//let two = false && true
+//let three = true && false
+//let four = true && true
+
+// 論理和
+let one = false || false
+let two = false || true
+let three = true || false
+let four = true || true
+print(one)
+print(two)
+print(three)
+print(four)
