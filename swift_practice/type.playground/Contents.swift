696150909edc6d855de8a1f723441d22c9ed5ec5
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-04 17:53:31 +0900

    swift2-7

--- swift_practice/type.playground/Contents.swift
+++ swift_practice/type.playground/Contents.swift
@@ -1,1 +1,17 @@
+// 基本的な型
+let string: String = "string"
+let int: Int = 0
+let double: Double = 0.0
+let bool: Bool = true // true or false
 
+print(string)
+print(int)
+print(double)
+print(bool)
+
+// 型推論
+let string2 = "string2"
+print(string2)
+
+var double2 = 0.0
+double2 = 0.5
