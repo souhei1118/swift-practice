a0cae043b1a8c7410cb3ce0019b78d7968be7574
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-05 01:25:54 +0900

    [finish]swift2-11

--- swift_practice/comparison2.playground/Contents.swift
+++ swift_practice/comparison2.playground/Contents.swift
@@ -1,1 +1,16 @@
+// 文字列の比較演算
+// ==　→右辺と左辺が一致する
+// != →右辺と左辺が一致しない
 
+let fruits1 = "りんご"
+let fruits2 = "ばなな"
+let eequalString = fruits1 != fruits2
+
+let one = 1
+let two = "2"
+let twoInt = Int(two)
+let oneString = String(one)
+let equalInt = one == twoInt
+let equalString2 = oneString == two
+let contents = "フルーツの中身は" + fruits1 + "と" + fruits2 + "です。"
+print(contents)
