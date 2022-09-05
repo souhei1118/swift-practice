14f97a53ce53b6082d810a825d91fc4f8ba37bd6
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-05 22:47:20 +0900

    [finish]swift3-14

--- swift_practice/if.playground/Contents.swift
+++ swift_practice/if.playground/Contents.swift
@@ -1,1 +1,20 @@
+// if分
+let isTrue = true
+if isTrue {
+    print("isTrueの値は「true」です。")
+}
 
+let number = 2
+if number == 1 {
+    print("numberは「１」です。")
+} else if number == 2 {
+    print("numberは「１」ではありません。")
+} else {
+    print("numberは「１」と「２」以外です")
+}
+
+// オプショナルバインディング
+let optionalInt: Int? = nil
+if let int = optionalInt {
+    print(int)
+}
