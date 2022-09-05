5faf5cb95c7ac3d4e7c38085f864b50d2dcda937
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-05 22:31:28 +0900

    [finish]swift2-12

--- swift_practice/array.playground/Contents.swift
+++ swift_practice/array.playground/Contents.swift
@@ -4,4 +4,8 @@ let a = stringArray[0]
 stringArray.append("d")
 stringArray.remove(at: 0)
 
-
+let mappedStirngArray = stringArray.map({$0 + "です。"})
+let intArray = [1,2,3]
+let mappedIntArray = intArray.map({$0 + 1})
+let isContain = mappedIntArray.contains(2)
+print(isContain)
