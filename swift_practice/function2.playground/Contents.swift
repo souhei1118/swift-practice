0b2a47ddb3c4da718d4bbf406bfcd88488973f7d
Souhei Takashi <so.takashi1118@gmail.com>
2022-09-06 23:46:50 +0900

    [finish]swift4-18

--- swift_practice/function2.playground/Contents.swift
+++ swift_practice/function2.playground/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+// 外部引数名と内部引数名
+func sum(_ first: Int, _ second: Int) -> Int {
+    return first + second
+}
+let first = 5
+let second = 10
+let sumNumber = sum(first,second)
 
-var greeting = "Hello, playground"
+// 引数のデフォルト値
+func say(text: String = "Hello!") {
+    print(text)
+}
+say(text: "Hello!")
