f274f2d793db2b449c921274712292863fb8090b
髙師蒼平 <so.takashi1118@gmail.com>
2023-05-28 16:05:05 +0900

    Enumの基本

---
+++ swift_practice/Enum.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+enum Signal {
+    case Blue
+    case Yellow
+    case Red
+}
+
+var sig: Signal
+sig = .Yellow
+
+switch sig {
+case .Blue:
+    print("Go!")
+case .Yellow:
+    print("Caution!")
+case .Red:
+    print("Stop")
+}
